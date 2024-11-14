b650802ea4b47ee5debb0c40ecf4b3ba9b87f6b0
nucl30n <trigon297@gmail.com>
2024-11-14 01:34:09 -0500

    Update load.js

--- chrome/load.js
+++ chrome/load.js
@@ -1,13 +1,11 @@
 function waitForElm(selector) {
   return new Promise(resolve => {
-    if (document.querySelector(selector)) resolve(document.querySelector(selector));
+    document.querySelector(selector) && resolve(document.querySelector(selector));
 
-    const observer = new MutationObserver(() => {
-      if (document.querySelector(selector)) {
-        resolve(document.querySelector(selector));
-        observer.disconnect();
-      }
-    });
+    const observer = new MutationObserver(() =>
+      document.querySelector(selector) && resolve(document.querySelector(selector)) && observer.disconnect()
+
+    );
 
     observer.observe(document.body, {
       childList: true,
@@ -20,13 +18,8 @@ function waitForElm(selector) {
   const div = document.createElement('div');
   div.id = 'sbx-panel';
   div.innerHTML = await fetch(chrome.runtime.getURL('panel.html')).then(res => res.text());
-
-  if (document.body.classList.contains('dark-theme')) div.classList.add('dark');
-
-  const linebreak = document.createElement('br');
-
   const runningGames = await waitForElm('#rbx-running-games');
 
   runningGames.parentNode.insertBefore(div, runningGames);
-  runningGames.parentNode.insertBefore(linebreak, runningGames);
 })();
+
